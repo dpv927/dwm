d0ddceaffb0a7be9eb99fc0ea7742d6087f0565f
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2023-09-26 12:04:56 +0200

    Add comment

--- update-src.sh
+++ update-src.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# This script is not designed for an external user to use,
+# but for the repository developers.
+#
+# The utility of this script is to update the contents of the 
+# src/ folder in the current directory, so that commits can be
+# made later.
+#                                                     by: @dpv927
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Get the script path
+spath="$(readlink -f "$0")"
+sdir="$(dirname "$spath")"
+
 # Define dwm working directories
 DWM_DIR="${HOME}/.config/dwm"
-GIT_DIR="$(pwd)"
+GIT_DIR="${sdir}/src"
 
 # Update repo files
 rm -rf "${GIT_DIR}/dwm"
-cp -r "${DWM_DIR}" "${GIT_DIR}"
+cp -r "${DWM_DIR}/*" "${GIT_DIR}"
 
 # Show changes
 git status
