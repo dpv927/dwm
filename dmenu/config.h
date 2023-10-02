bd9b654807aed22402ea56a5c2e76cf3171a82cd
dpv927 <dpv927@inlumine.ual.es>
2023-10-02 18:52:00 +0200

    Add dmenu configuration

---
+++ dmenu/config.h
@@ -0,0 +1,17 @@
+#include "themes/catppuccin.h"
+
+/* Patch: bar height */
+static const int user_bt = 20;
+
+/* Default variables */ 
+static unsigned int lines   = 0;
+static int topbar           = 1;
+static const char worddelimiters[] = " ";
+static const char* prompt = NULL;
+
+/* Color scheme */
+static const char *colors[SchemeLast][2] = {
+	[SchemeNorm] = { NormForeground, NormBackground },
+	[SchemeSel]  = { SelForeground,  SelBackground  },
+	[SchemeOut]  = { OutForeground,  OutBackground  },
+};
